e3e4fd8ec3105771d4a7ab57677ab055f7df939f
Muhammad Ahsan <mahsan12030@gmail.com>
2025-12-31 12:56:17 +0500

    Backend database modules added

---
+++ backend/app/db/base.py
@@ -0,0 +1,6 @@
+from app.db.base_class import Base
+from app.models.user import User
+from app.models.subreddit import Subreddit
+from app.models.post import Post
+from app.models.comment import Comment
+from app.models.vote import Vote
