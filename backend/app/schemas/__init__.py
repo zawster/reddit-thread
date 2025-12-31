1c2ea1f6231b81d81997d578ee4f258942b2cfa8
Muhammad Ahsan <mahsan12030@gmail.com>
2025-12-31 12:57:04 +0500

    Backend db schemas added

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .user import User, UserCreate, UserUpdate, Token, TokenPayload
+from .subreddit import Subreddit, SubredditCreate, SubredditUpdate
+from .post import Post, PostCreate, PostUpdate
+from .comment import Comment, CommentCreate, CommentUpdate
+from .vote import VoteBase, VoteCreate, VoteResponse
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+# Shared properties
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    full_name: Optional[str] = None
+    username: Optional[str] = None
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    email: EmailStr
+    username: str
+    password: str
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+    
+    class Config:
+        from_attributes = True
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    pass
+
+# Additional properties stored in DB
+class UserInDB(UserInDBBase):
+    hashed_password: str
+
+# Token
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenPayload(BaseModel):
+    sub: Optional[int] = None
